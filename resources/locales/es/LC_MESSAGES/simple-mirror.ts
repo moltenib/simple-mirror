ec8ea08070c92511166fdb83153a87124b1ca4cd
Bruno <stamby@github.com>
2026-02-24 16:29:32 +0100

    Refactor

---
+++ resources/locales/es/LC_MESSAGES/simple-mirror.ts
@@ -0,0 +1,139 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="es_ES">
+<context>
+    <name>DirectoryChooserWidget</name>
+    <message>
+        <source>Select</source>
+        <translation>Seleccionar</translation>
+    </message>
+</context>
+<context>
+    <name>MainWindow</name>
+    <message>
+        <source>Simple Mirror</source>
+        <translation>Simple Mirror</translation>
+    </message>
+    <message>
+        <source>Origin:</source>
+        <translation>Origen:</translation>
+    </message>
+    <message>
+        <source>The folder to be backed up.</source>
+        <translation>La carpeta que se va a respaldar.</translation>
+    </message>
+    <message>
+        <source>Select origin folder</source>
+        <translation>Seleccionar carpeta de origen</translation>
+    </message>
+    <message>
+        <source>Destination:</source>
+        <translation>Destino:</translation>
+    </message>
+    <message>
+        <source>A folder inside a back-up medium.</source>
+        <translation>Una carpeta dentro de un medio de copia de seguridad.</translation>
+    </message>
+    <message>
+        <source>Select destination folder</source>
+        <translation>Seleccionar carpeta de destino</translation>
+    </message>
+    <message>
+        <source>Synchronize</source>
+        <translation>Sincronizar</translation>
+    </message>
+    <message>
+        <source>Stop</source>
+        <translation>Detener</translation>
+    </message>
+    <message>
+        <source>Idle</source>
+        <translation>Inactivo</translation>
+    </message>
+    <message>
+        <source>Stopped</source>
+        <translation>Detenido</translation>
+    </message>
+    <message>
+        <source>Done</source>
+        <translation>Completado</translation>
+    </message>
+    <message>
+        <source>Failed</source>
+        <translation>Fallido</translation>
+    </message>
+    <message>
+        <source>Synchronization failed with exit code %1.</source>
+        <translation>La sincronización falló con el código de salida %1.</translation>
+    </message>
+    <message>
+        <source>Synchronization failed</source>
+        <translation>Sincronización fallida</translation>
+    </message>
+    <message>
+        <source>Running...</source>
+        <translation>En ejecución...</translation>
+    </message>
+    <message>
+        <source>Stopping...</source>
+        <translation>Deteniendo...</translation>
+    </message>
+    <message>
+        <source>rsync not found</source>
+        <translation>rsync no encontrado</translation>
+    </message>
+    <message>
+        <source>Error</source>
+        <translation>Error</translation>
+    </message>
+    <message>
+        <source>Error: %1
+</source>
+        <translation>Error: %1
+</translation>
+    </message>
+    <message>
+        <source>Synchronization error</source>
+        <translation>Error de sincronización</translation>
+    </message>
+    <message>
+        <source>Notice</source>
+        <translation>Aviso</translation>
+    </message>
+    <message>
+        <source>Any files in the destination folder that do not exist in the origin will be deleted.</source>
+        <translation>Se eliminarán los archivos en la carpeta de destino que no existan en el origen.</translation>
+    </message>
+    <message>
+        <source>This is to keep the destination folder up to date. Continue?</source>
+        <translation>Esto mantiene actualizada la carpeta de destino. ¿Continuar?</translation>
+    </message>
+    <message>
+        <source>Please choose both origin and destination folders.</source>
+        <translation>Seleccione las carpetas de origen y destino.</translation>
+    </message>
+    <message>
+        <source>Origin folder does not exist: %1</source>
+        <translation>La carpeta de origen no existe: %1</translation>
+    </message>
+    <message>
+        <source>Destination folder does not exist: %1</source>
+        <translation>La carpeta de destino no existe: %1</translation>
+    </message>
+</context>
+<context>
+    <name>RsyncRunner</name>
+    <message>
+        <source>A synchronization is already running.</source>
+        <translation>Ya hay una sincronización en ejecución.</translation>
+    </message>
+    <message>
+        <source>SIMPLE_MIRROR_RSYNC is set but does not point to a valid file: %1</source>
+        <translation>SIMPLE_MIRROR_RSYNC está definida pero no apunta a un archivo válido: %1</translation>
+    </message>
+    <message>
+        <source>Could not find rsync. Set SIMPLE_MIRROR_RSYNC, add rsync to PATH, or bundle &quot;runtime/bin/rsync&quot; on Linux, or &quot;runtime/msys2/usr/bin/rsync.exe&quot; on Windows.</source>
+        <translation>No se pudo encontrar rsync. Defina SIMPLE_MIRROR_RSYNC, añada rsync al PATH o incluya &quot;runtime/bin/rsync&quot; en Linux o &quot;runtime/msys2/usr/bin/rsync.exe&quot; en Windows.</translation>
+    </message>
+</context>
+</TS>
